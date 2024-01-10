db4712471072b95fffd28f7d7cf72a457befd04c
Eli Manzo <96manzo.eli@gmail.com>
2024-01-09 19:03:20 -0800

    Adding states/reducers

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useReducer } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+  }
+  return state;
+}
+
+function makeInitialState() {
+  return {};
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, null, makeInitialState());
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{JSON.stringify(state, null, 2)}</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
