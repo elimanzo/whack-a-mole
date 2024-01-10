87d4b94a3ed109fd7961b4422c015d0ad06ea664
Eli Manzo <96manzo.eli@gmail.com>
2024-01-10 01:19:06 -0800

    Adding Mole SVG

--- Cell.js
+++ Cell.js
@@ -1,16 +1,18 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { SvgXml } from 'react-native-svg';
 
+import moleSvg from './moleSvg.js';
+
+function MoleSVG() {
+  return <SvgXml xml={moleSvg} width='80%' height='80%' />;
+}
 export default function Cell({ cellValue }) {
-  return (
-    <View style={styles.container}>
-      <Text>{cellValue}</Text>
-    </View>
-  );
+  return <View style={styles.container}>{cellValue && <MoleSVG />}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: 80,
+    width: 100,
     aspectRatio: 1,
     bordeRadius: 8,
     backgroundColor: 'lightblue',
